117cfc7cac06287572be32a9902e556d5fff6a72
Abdullah Asad <abdullahbinasad14@gmail.com>
2017-12-17 18:18:02 -0500

    Cleaned up the code and centralized all collision detection.

--- server/Player.js
+++ server/Player.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Player = /** @class */ (function () {
-    function Player(id, name) {
+class Player {
+    constructor(id, name) {
         this.x = 400;
         this.y = 400;
         this.r = 0;
@@ -9,6 +9,5 @@ var Player = /** @class */ (function () {
         this.id = id;
         this.name = name;
     }
-    return Player;
-}());
+}
 exports.Player = Player;
